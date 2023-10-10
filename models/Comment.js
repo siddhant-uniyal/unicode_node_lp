c7bd7e41cca875b33a9e3ea2f28952a9ded1e165
Siddhant Uniyal <siddhantuniyal416@gmail.com>
2023-10-10 10:50:43 +0530

    Changed body field name to comment

--- models/Comment.js
+++ models/Comment.js
@@ -19,7 +19,7 @@ const schema = new mongoose.Schema({
         required : true
     },
 
-    body : {
+    comment : {
         type : String, //actual content of comment
         required : true
     },

595f547ad80739422681c8aafb7f1014f05e65a9
Siddhant Uniyal <110982730+siddhant-uniyal@users.noreply.github.com>
2025-06-27 01:18:10 +0530

    fix: json in auth middleware response

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -9,7 +9,10 @@ const isAuthenticated = async(req,res,next)=>{
         req.user = user;
         next();
     }catch(err){
-        return res.status(400).send(err.message);
+        return res.status(400).json({
+            "success" : false,
+            "message" : err
+        });
     }
 }
 
